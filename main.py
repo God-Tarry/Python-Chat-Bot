6784f532e0dd2526d0b847373c7a7c55388b9797
Godfrey Tarry <godfreyofficial7@gmail.com>
2022-04-10 20:55:49 +0200

    .april 10 chatbot tut 04

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http.client import responses
 import nltk
 nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
@@ -10,6 +11,7 @@ import tensorflow
 ops.reset_default_graph() """
 import random
 import json
+import pickle
 
 with open("intents.json") as file:
     data = json.load(file)
@@ -17,74 +19,82 @@ with open("intents.json") as file:
 # print(data)
 
 # empty lists
+try:
+    with open("date.pickle", "rb") as f:
+        words,labels,training,output = pickle.load(f)
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-
-
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
-
-
-
-words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
-
-# turns words list into a set - takes all the words make sure there are no duplicates
-
-# list converts the words back into a list
-
-words = sorted(list(set(words)))
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
 
-# sorts the labels aswell
-
-labels =sorted(labels)
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
 
 
-# neural networks only understand numbers
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
 
-""" 
-    to train our model we feed a list - 
-    each position in the list will represent weather a word exists or not 
-"""
+    # turns words list into a set - takes all the words make sure there are no duplicates
 
-# variables /lists for the training and output
+    # list converts the words back into a list
 
-training = []
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
+    words = sorted(list(set(words)))
 
 
-for x, doc in enumerate(docs_x):
-    bag =[]
+    # sorts the labels aswell
 
-    wrds = [stemmer.stem(w) for w in doc]
-
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-    training.append(bag)
-    output.append(output_row)
+    labels =sorted(labels)
 
 
-training = numpy.array(training)
-output = numpy.array(output)
+
+    # neural networks only understand numbers
+
+    """ 
+        to train our model we feed a list - 
+        each position in the list will represent weather a word exists or not 
+    """
+
+    # variables /lists for the training and output
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+
+    for x, doc in enumerate(docs_x):
+        bag =[]
+
+        wrds = [stemmer.stem(w) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words,labels,training, output),f)
+
 
 
 """ tf-learn start coding the model """
@@ -104,5 +114,47 @@ net  = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+                
+    return numpy.array(bag)
+
+
+def chat():
+    print("start talking with the bot! (type quite to stop)")
+
+    while True:
+        inp = input("You: ")
+        if inp.lower == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+        # print(tag)
+
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg["responses"]
+
+        print(random.choice(responses))
+
+
+chat()
